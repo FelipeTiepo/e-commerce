e0b68d6c026315061c8231b2629a59bb072bc1e5
unknown <ftiepo20@gmail.com>
2024-06-26 14:28:56 -0300

    adicionando o mongodb, acesso ao google cloud e criando as paginas principais

---
+++ pages/pedidos.js
@@ -0,0 +1,7 @@
+import Layout from "@/components/Layout";
+
+export default function Pedidos() {
+    return (
+        <Layout>Pedidos page</Layout>
+    )
+}
---
+++ pages/produtos.js
@@ -0,0 +1,7 @@
+import Layout from "@/components/Layout";
+
+export default function Produtos() {
+    return (
+        <Layout>producs page</Layout>
+    )
+}
